5de9dd9d431ed5943d1e53a9caead57a885980dc
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-17 22:07:15 -0400

    Created placeholder files
    Created Display and PresentationFramework libraries

---
+++ libraries/Display/Display.h
@@ -0,0 +1,12 @@
+//  Created by Andrew Florjancic on 9/17/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef Display_h
+#define Display_h
+
+// User interface component to interact with a LCD screen.
+class Display {
+    public:
+        Display();
+};
+
+#endif
---
+++ libraries/PresentableFramework/Feature.cpp
@@ -0,0 +1,5 @@
+//  Created by Andrew Florjancic on 9/17/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#include "Feature.h"
+
+Feature::Feature() { }
