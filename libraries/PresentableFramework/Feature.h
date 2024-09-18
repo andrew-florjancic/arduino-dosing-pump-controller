e5d60d3b4f9e054245ed7b8282b44225a983702a
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-17 22:07:15 -0400

    Created placeholder files
    Created Display and PresentationFramework libraries

---
+++ libraries/PresentableFramework/Feature.h
@@ -0,0 +1,12 @@
+//  Created by Andrew Florjancic on 9/17/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef Feature_h
+#define Feature_h
+
+// A Feature that can show content to the user.
+class Feature {
+    public:
+        Feature();
+};
+
+#endif
---
+++ libraries/PresentableFramework/Presentable.h
@@ -0,0 +1,11 @@
+//  Created by Andrew Florjancic on 9/17/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef Presentable_h
+#define Presentable_h
+
+class Presentable {
+    public:
+        Presentable();
+};
+
+#endif
