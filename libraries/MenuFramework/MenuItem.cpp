2d0dc961169c3fe37b265c3e21fd1ffdcb3520c4
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-18 18:47:27 -0400

    Created MenuItem
    Added a MenuItem class that is a Presentable with a title and item type.
    These will be used to create menus, submenus, and features in a menu

---
+++ libraries/MenuFramework/MenuItem.cpp
@@ -0,0 +1,5 @@
+//  Created by Andrew Florjancic on 9/18/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#include "MenuItem.h"
+
+MenuItem::MenuItem(String title, ItemType item_type): title(title), item_type(item_type), Presentable() { }
