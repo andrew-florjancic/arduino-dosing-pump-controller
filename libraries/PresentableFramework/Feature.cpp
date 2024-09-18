b009d234e48b2cacd50fd0d89e6206b9fb2128e8
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-17 23:39:58 -0400

    Created a Feature class
    Basic Feature class that has a display object and methods to show its feature or show an error.

--- libraries/PresentableFramework/Feature.cpp
+++ libraries/PresentableFramework/Feature.cpp
@@ -3,3 +3,10 @@
 #include "Feature.h"
 
 Feature::Feature() { }
+
+void Feature::showError(uint8_t code) {
+    display.lcd.clear();
+    display.lcd.print(error_message);
+    display.lcd.setCursor(0,1);
+    display.lcd.print(error_code_message + String(code));
+}
--- libraries/PresentableFramework/Feature.h
+++ libraries/PresentableFramework/Feature.h
@@ -3,10 +3,29 @@
 #ifndef Feature_h
 #define Feature_h
 
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Display.h>
+
 // A Feature that can show content to the user.
 class Feature {
     public:
+        // Constructor creates a new feature.
         Feature();
+    protected:
+        // The Display object used to show Feature content to the user.
+        Display display;
+
+        // Displays the feature.
+        virtual void showFeature() = 0;
+
+        // Displays an error message with an error code.
+        // @param code: The error code to display.
+        void showError(uint8_t code);
+    private:
+        // Error message to be displayed alerting the user an error has occured.
+        const String error_message = "Error!";
+        const String error_code_message = "Code: ";
 };
 
 #endif
