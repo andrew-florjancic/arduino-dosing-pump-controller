43e585fd5032442cf81b4fb9be14cd5883e2a426
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-18 23:19:49 -0400

    Created StorageManager class placeholder

---
+++ dosing_pump_controller/StorageManager.cpp
@@ -0,0 +1,8 @@
+//  Created by Andrew Florjancic on 9/18/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#include <EEPROM.h>
+#include "StorageManager.h"
+
+StorageManager::StorageManager() {
+    // TODO: get saved data from EEPROM or use default data
+}
---
+++ dosing_pump_controller/StorageManager.h
@@ -0,0 +1,16 @@
+//  Created by Andrew Florjancic on 9/18/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef StorageManager_h
+#define StorageManager_h
+
+#include <Arduino.h>
+
+// Manages device storage
+class StorageManager {
+    public:
+
+        // TODO:
+        StorageManager();
+
+};
+#endif
