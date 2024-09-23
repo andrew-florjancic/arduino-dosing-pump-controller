0b1634a16800685776977dd8a0750f295091d6af
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-23 13:11:58 -0400

    Created ResetPumpSettingsMenuItem
    Added a Feature to allow a user to reset pump settings and confirm or cancel the reset procedure.

---
+++ dosing_pump_controller/ResetPumpSettingsMenuItem.cpp
@@ -0,0 +1,30 @@
+//  Created by Andrew Florjancic on 9/23/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved.
+#include "ResetPumpSettingsMenuItem.h"
+
+ResetPumpSettingsMenuItem::ResetPumpSettingsMenuItem(PumpController& pump_controller) : MenuItem("Reset", false), pump_controller(pump_controller) {}
+
+void ResetPumpSettingsMenuItem::showFeature() {
+    display->lcd.clear();
+    display->lcd.print(confirmation_message);
+    display->lcd.print(confirm_reset ? yes_message : no_message);
+}
+
+void ResetPumpSettingsMenuItem::presentableWillDismiss() {
+    confirm_reset = true;
+}
+
+void ResetPumpSettingsMenuItem::leftAction() {
+    confirm_reset = false;
+}
+
+void ResetPumpSettingsMenuItem::rightAction() {
+    confirm_reset = true;
+}
+
+void ResetPumpSettingsMenuItem::selectAction() {
+    if(confirm_reset) {
+        pump_controller.resetPumpSettings();
+    }
+    completePresentation();
+}
---
+++ dosing_pump_controller/ResetPumpSettingsMenuItem.h
@@ -0,0 +1,37 @@
+//  Created by Andrew Florjancic on 9/23/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef ResetPumpSettingsMenuItem_h
+#define ResetPumpSettingsMenuItem_h
+
+#include <Arduino.h>
+#include <MenuItem.h>
+#include "PumpController.h"
+
+// A MenuItem that allows the user to reset a pumps settings to the factory default values.
+class ResetPumpSettingsMenuItem : public MenuItem {
+    public:
+        // Constructor creates a ResetPumpSettingsMenuItem.
+        ResetPumpSettingsMenuItem(PumpController& pump_controller);
+    private:
+        const PumpController pump_controller; // The PumpController used to reset pump settings.
+        const String confirmation_message = "Confirm: "; // A Feature message to be displayed to the user when confirming pump reset.
+        const String yes_message = "Yes"; // A Feature message to be displayed to the user when they have selected to confirm the reset.
+        const String no_message = "No"; // A Feature message to be displayed to the user when they have selected to cancel the reset.
+        bool confirm_reset = true; // Confirmiation from the user that the pump settings should be reset. Default to true.
+
+        // Displays the current value of the confirm reset boolean.
+        void showFeature();
+
+        // Resets the confrim reset state.
+        void presentableWillDismiss();
+
+        // Updates the confirm reset value to false.
+        void leftAction();
+
+        // Updates the confirm reset value to true.
+        void rightAction();
+
+        // Resets the pump settings if the confirm reset value is true then dismisses the Feature.
+        void selectAction();
+};
+#endif
