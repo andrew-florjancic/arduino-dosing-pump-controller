5e0579e8ff8c5549659f9e21e2c6308ca984d434
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-22 21:20:05 -0400

    Created ReturnMenuItem
    Also added return menu items to the menus in the main sketch

---
+++ dosing_pump_controller/ReturnMenuItem.cpp
@@ -0,0 +1,15 @@
+//  Created by Andrew Florjancic on 9/22/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved.
+#include "ReturnMenuItem.h"
+
+ReturnMenuItem::ReturnMenuItem() : MenuItem("Return", true) {}
+
+void ReturnMenuItem::showFeature() {}
+
+void ReturnMenuItem::presentableWillDismiss() {}
+
+void ReturnMenuItem::leftAction() {}
+
+void ReturnMenuItem::rightAction() {}
+
+void ReturnMenuItem::selectAction() {}
---
+++ dosing_pump_controller/ReturnMenuItem.h
@@ -0,0 +1,29 @@
+//  Created by Andrew Florjancic on 9/22/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved
+#ifndef ReturnMenuItem_h
+#define ReturnMenuItem_h
+
+#include <MenuItem.h>
+
+// A special MenuItem only used by a Menu to return control to the supermenu. This item should never actually be presented.
+class ReturnMenuItem : public MenuItem {
+    private:
+        // Empty method, should never actually be executed
+        void showFeature();
+
+        // Empty method, should never actually be executed
+        void presentableWillDismiss();
+        
+        // Empty method, should never actually be executed
+        void leftAction();
+        
+        // Empty method, should never actually be executed
+        void rightAction();
+
+        // Empty method, should never actually be executed
+        void selectAction(); 
+    public:
+        // Constructor creates a ReturnMenuItem.
+        ReturnMenuItem();
+};
+#endif
