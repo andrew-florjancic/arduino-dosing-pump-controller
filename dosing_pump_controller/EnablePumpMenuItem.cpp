77bd54e90b461017f9ecf6570d7fa32a28a7a32b
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-09-29 15:03:01 -0400

    Created EnablePumpMenuItem
    Added a feature to set the dosing enabled pump setting

---
+++ dosing_pump_controller/EnablePumpMenuItem.cpp
@@ -0,0 +1,28 @@
+//  Created by Andrew Florjancic on 9/29/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved.
+#include "EnablePumpMenuItem.h"
+
+EnablePumpMenuItem::EnablePumpMenuItem(PumpController& pump_controller) : MenuItem("Enable Pump", false), pump_controller(pump_controller) {
+    is_enabled = pump_controller.getDosingEnabled();
+}
+
+void EnablePumpMenuItem::showFeature() {
+    display->lcd.clear();
+    display->lcd.print(input_message);
+    display->lcd.print(is_enabled ? on_message : off_message);
+}
+
+void EnablePumpMenuItem::presentableWillDismiss() {}
+
+void EnablePumpMenuItem::leftAction() {
+    is_enabled = false;
+}
+
+void EnablePumpMenuItem::rightAction() {
+    is_enabled = true;
+}
+
+void EnablePumpMenuItem::selectAction() {
+    pump_controller.updateDosingEnabled(is_enabled);
+    completePresentation();
+}
