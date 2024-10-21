ec48d56185d5bc9ccff177a178768c878de6a730
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-10-21 13:17:43 -0400

    cleanup board struct and removed const variables

--- dosing_pump_controller/dosing_pump_controller.ino
+++ dosing_pump_controller/dosing_pump_controller.ino
@@ -22,55 +22,58 @@
 #include "src/MenuItems/ComingSoonMenuItem.h"
 
 // A model of the Arduino Nano Every board being used for the dosing pump controller.
-const struct Board {
+struct Board {
   // Pin numbers with a description of their useage.
   const struct Pins {
-    const uint8_t d13 = 13;  // Pin #1: D13 - unused 
-                             // Pin #2: +3V - unused
-                             // Pin #3: AREF - unused
-    const uint8_t d14 = 14;  // Pin #4: D14 - Connected to LCD pin#6 EN
-    const uint8_t d15 = 15;  // Pin #5: D15 - Connected to LCD pin#11 D4
-    const uint8_t d16 = 16;  // Pin #6: D16 - Connected to LCD pin#12 D5
-    const uint8_t d17 = 17;  // Pin #7: D17 - Connected to LCD pin#13 D6
-    const uint8_t d18 = 18;  // Pin #8: D18 - Connected to LCD pin#14 D7
-    const uint8_t d19 = 19;  // Pin #9: D19 - Connected to Rotary Encoder ENC_B
-    const uint8_t d20 = 20;  // Pin #10: D20 - Connected to Rotary Encoder ENC_A
-    const uint8_t d21 = 21;  // Pin #11: D21 - Connected to Rotary Encoder switch
-                             // Pin #12: +5V - Connected to positive rail
-                             // Pin #13: Reset - unused
-                             // Pin #14: GND - Connected to negative rail
-                             // Pin #15: VIN - Connected to power supply positive wire
-                             // Pin #16: TX - unused
-                             // Pin #17: RX - unused
-                             // Pin #18: Reset - unused
-                             // Pin #19: GND - unused
-    const uint8_t d2 = 2;    // Pin #20: D2 - unused
-    const uint8_t d3 = 3;    // Pin #21: D3 PWM  - Connected to LCD pin#15 to control brightness
-    const uint8_t d4 = 4;    // Pin #22: D4 - unused
-    const uint8_t d5 = 5;    // Pin #23: D5 PWM - Connected to LCD pin#3 to control contrast
-    const uint8_t d6 = 6;    // Pin #24: D6 PWM - Connected to Motor1 IN1
-    const uint8_t d7 = 7;    // Pin #25: D7 - unused
-    const uint8_t d8 = 8;    // Pin #26: D8 - unused
-    const uint8_t d9 = 9;    // Pin #27: D9 PWM - Connected to Motor2 IN1
-    const uint8_t d10 = 10;  // Pin #28: D10 PWM - Connected to Motor3 IN1
-    const uint8_t d11 = 11;  // Pin #29: D11 - unused
-    const uint8_t d12 = 12;  // Pin #30: D12 - Connected to LCD pin#4
+    uint8_t d13 = 13,  // Pin #1: D13 - unused 
+                       // Pin #2: +3V - unused
+                       // Pin #3: AREF - unused
+            d14 = 14,  // Pin #4: D14 - Connected to LCD pin#6 EN
+            d15 = 15,  // Pin #5: D15 - Connected to LCD pin#11 D4
+            d16 = 16,  // Pin #6: D16 - Connected to LCD pin#12 D5
+            d17 = 17,  // Pin #7: D17 - Connected to LCD pin#13 D6
+            d18 = 18,  // Pin #8: D18 - Connected to LCD pin#14 D7
+            d19 = 19,  // Pin #9: D19 - Connected to Rotary Encoder ENC_B
+            d20 = 20,  // Pin #10: D20 - Connected to Rotary Encoder ENC_A
+            d21 = 21,  // Pin #11: D21 - Connected to Rotary Encoder switch
+                       // Pin #12: +5V - Connected to positive rail
+                       // Pin #13: Reset - unused
+                       // Pin #14: GND - Connected to negative rail
+                       // Pin #15: VIN - Connected to power supply positive wire
+                       // Pin #16: TX - unused
+                       // Pin #17: RX - unused
+                       // Pin #18: Reset - unused
+                       // Pin #19: GND - unused
+            d2 = 2,    // Pin #20: D2 - unused
+            d3 = 3,    // Pin #21: D3 PWM  - Connected to LCD pin#15 to control brightness
+            d4 = 4,    // Pin #22: D4 - unused
+            d5 = 5,    // Pin #23: D5 PWM - Connected to LCD pin#3 to control contrast
+            d6 = 6,    // Pin #24: D6 PWM - Connected to Motor1 IN2
+            d7 = 7,    // Pin #25: D7 - unused
+            d8 = 8,    // Pin #26: D8 - unused
+            d9 = 9,    // Pin #27: D9 PWM - Connected to Motor2 IN2
+            d10 = 10,  // Pin #28: D10 PWM - Connected to Motor3 IN2
+            d11 = 11,  // Pin #29: D11 - unused
+            d12 = 12;  // Pin #30: D12 - Connected to LCD pin#4
   } pins;
 
-  const StorageManager storage_manager;
-  const RotaryEncoder rotary_encoder = RotaryEncoder(pins.d19, pins.d20, pins.d21, 150);
-  const Display display = Display(LiquidCrystal(pins.d12, pins.d14, pins.d15, pins.d16, pins.d17, pins.d18), 2, 16, &pins.d3, &pins.d5);
-  const DisplayController display_controller = DisplayController(display, storage_manager);
+  StorageManager storage_manager = StorageManager();
+  RotaryEncoder rotary_encoder = RotaryEncoder(pins.d19, pins.d20, pins.d21, 150);
+  Display display = Display(LiquidCrystal(pins.d12, pins.d14, pins.d15, pins.d16, pins.d17, pins.d18), 2, 16, &pins.d3, &pins.d5);
+  DisplayController display_controller = DisplayController(display, storage_manager);
   Menu main_menu = Menu("Main Menu");
   Menu settings_menu = Menu("Settings");
-
+  
+  Motor motor1 = Motor(nullptr, &pins.d6),
+        motor2 = Motor(nullptr, &pins.d9),
+        motor3 = Motor(nullptr, &pins.d10);
   // A PumpController and a Menu.
-  const struct DosingPump {
-    const PumpController controller;
-    const Menu menu;
-  } pump1 = {PumpController(StorageManager::PumpID::pump_1, storage_manager, Motor(&pins.d6, nullptr)), Menu("Pump 1")},
-    pump2 = {PumpController(StorageManager::PumpID::pump_2, storage_manager, Motor(&pins.d9, nullptr)), Menu("Pump 2")},
-    pump3 = {PumpController(StorageManager::PumpID::pump_3, storage_manager, Motor(&pins.d10, nullptr)), Menu("Pump 3")};
+  struct DosingPump {
+    PumpController controller;
+    Menu menu;
+  } pump1 = {PumpController(StorageManager::PumpID::pump_1, storage_manager, motor1), Menu("Pump 1")},
+    pump2 = {PumpController(StorageManager::PumpID::pump_2, storage_manager, motor2), Menu("Pump 2")},
+    pump3 = {PumpController(StorageManager::PumpID::pump_3, storage_manager, motor3), Menu("Pump 3")};
 } board;
 
 // Create calibrate pump menu items. These need to be created now so they can be called by the return control functions.
@@ -113,14 +116,14 @@ void encoderBInput() { board.rotary_encoder.encodeB(); }
 // A callback function the RotaryEncoder will call every time a RotaryEncoder Action is detected.
 void actionDetected(RotaryEncoder::Actions action) { board.main_menu.sendAction(action); }
 
-void addPumpMenuItems(const Menu& menu, CalibratePumpMenuItem& calibrate_pump, void (*return_control)()) {
+void addPumpMenuItems(PumpController& pump_controller, Menu& menu, CalibratePumpMenuItem& calibrate_pump, void (*return_control)()) {
   menu.addMenuItem(new ReturnMenuItem(), return_control);
   menu.addMenuItem(new ComingSoonMenuItem("Info"), return_control);
-  menu.addMenuItem(new SetDoseMenuItem(board.pump1.controller), return_control);
-  menu.addMenuItem(new EnablePumpMenuItem(board.pump1.controller), return_control);
-  menu.addMenuItem(new PrimePumpMenuItem(board.pump1.controller), return_control);
+  menu.addMenuItem(new SetDoseMenuItem(pump_controller), return_control);
+  menu.addMenuItem(new EnablePumpMenuItem(pump_controller), return_control);
+  menu.addMenuItem(new PrimePumpMenuItem(pump_controller), return_control);
   menu.addMenuItem(&calibrate_pump, return_control);
-  menu.addMenuItem(new ResetPumpSettingsMenuItem(board.pump1.controller), return_control);
+  menu.addMenuItem(new ResetPumpSettingsMenuItem(pump_controller), return_control);
 }
 
 void setup() {
@@ -141,9 +144,9 @@ void setup() {
   board.main_menu.addMenuItem(&board.settings_menu, &mainMenuReturnControl);
 
   // Add items to the pump menus.
-  addPumpMenuItems(board.pump1.menu, calibrate_pump1, &pump1MenuReturnControl);
-  addPumpMenuItems(board.pump2.menu, calibrate_pump2, &pump2MenuReturnControl);
-  addPumpMenuItems(board.pump3.menu, calibrate_pump3, &pump3MenuReturnControl);
+  addPumpMenuItems(board.pump1.controller, board.pump1.menu, calibrate_pump1, &pump1MenuReturnControl);
+  addPumpMenuItems(board.pump2.controller, board.pump2.menu, calibrate_pump2, &pump2MenuReturnControl);
+  addPumpMenuItems(board.pump3.controller, board.pump3.menu, calibrate_pump3, &pump3MenuReturnControl);
 
   //Add items to the settings_menu
   board.settings_menu.addMenuItem(new ReturnMenuItem(), &settingsMenuReturnControl);
